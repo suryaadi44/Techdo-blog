a09c6f554a4044dbf9cdc0cc8b701c837eaa9c99
Oka Darmayasa <okadarmayasa10@gmail.com>
2022-05-04 21:20:21 +0800

    [#1] Adding sessions table and editing some other tables.

--- db/migrations/20220504102845_create_ddl_table.up.sql
+++ db/migrations/20220504102845_create_ddl_table.up.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
 	uid INT NOT NULL AUTO_INCREMENT,
 	username VARCHAR(20) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+    PRIMARY KEY(uid)
+)ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS user_details(
+	uid INT NOT NULL,
     email VARCHAR(25) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20),
@@ -9,13 +13,20 @@ CREATE TABLE IF NOT EXISTS users (
     phone VARCHAR(15),
     about_me TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(uid)
+    FOREIGN KEY (uid) REFERENCES users(uid)
 )ENGINE=InnoDB;
+CREATE TABLE sessions(
+	token INT NOT NULL,
+	uid INT NOT NULL, 
+	expireAt DATETIME,
+	PRIMARY KEY (token),
+	FOREIGN KEY (uid) REFERENCES users(uid)
+)
 CREATE TABLE blog_posts(
 	post_id INT NOT NULL AUTO_INCREMENT,
     author_id INT UNIQUE,
     banner VARCHAR(50) NOT NULL,
-    title VARCHAR(20) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     body TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (post_id),
