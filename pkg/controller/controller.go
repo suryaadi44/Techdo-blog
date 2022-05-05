99ab530cbcac1e320b96b2b9301914541f58fc35
suryaadi44 <suryawarior44@gmail.com>
2022-05-06 04:58:47 +0800

    [#11] create code scaffolding for viewing page

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 	authControllerPkg "github.com/suryaadi44/Techdo-blog/internal/auth/controller"
 	authServicePkg "github.com/suryaadi44/Techdo-blog/internal/auth/service"
+	postControllerPkg "github.com/suryaadi44/Techdo-blog/internal/post/controller"
 )
 
 func InitializeController(router *mux.Router, db *sql.DB) {
@@ -17,4 +18,7 @@ func InitializeController(router *mux.Router, db *sql.DB) {
 	AuthService := authServicePkg.NewUserAuthService(db)
 	AuthController := authControllerPkg.NewController(router, AuthService)
 	AuthController.InitializeController()
+
+	PostController := postControllerPkg.NewController(router)
+	PostController.InitializeController()
 }
