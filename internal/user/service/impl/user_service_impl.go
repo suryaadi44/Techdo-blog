664da927e3b7d56dee49493e2326cdfcb1d4c399
suryaadi <suryawarior44@gmail.com>
2022-05-10 09:52:05 +0800

    [#15] refactor: change query to get mini user detail

--- internal/user/service/impl/user_service_impl.go
+++ internal/user/service/impl/user_service_impl.go
@@ -14,7 +14,7 @@ type UserServiceImpl struct {
 func (u UserServiceImpl) GetUserMiniDetail(ctx context.Context, id int64) (dto.MiniUserDetail, error) {
 	var userDetail dto.MiniUserDetail
 
-	user, err := u.Repository.GetUserDetail(ctx, id)
+	user, err := u.Repository.GetUserMiniDetail(ctx, id)
 	if err != nil {
 		log.Println("[ERROR] GetUserMiniDetail: Error geting user detail-> error:", err)
 		return userDetail, err
