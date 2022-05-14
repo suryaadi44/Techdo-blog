6765f29d8b22677465ccfa42d37b0e193da71666
suryaadi <suryawarior44@gmail.com>
2022-05-14 14:47:31 +0800

    [#60] feat: finish implementation of pagination for dashboard

--- pkg/utils/paginator.go
+++ pkg/utils/paginator.go
@@ -1,15 +1,13 @@
 package utils
 
-import "fmt"
-
 type PageNavigation struct {
 	Max      int64
-	Active   string
-	PageList []string
+	Active   int64
+	PageList []int64
 }
 
 func Paginate(current int64, total int64) PageNavigation {
-	var stringRange []string
+	var stringRange []int64
 	var intRange []int64
 	var delta int64 = 2
 	var i, l int64
@@ -27,19 +25,19 @@ func Paginate(current int64, total int64) PageNavigation {
 	for idx, i := range intRange {
 		if idx != 0 {
 			if i-l == 2 {
-				stringRange = append(stringRange, fmt.Sprint(l+1))
+				stringRange = append(stringRange, l+1)
 			} else if i-l != 1 {
-				stringRange = append(stringRange, "...")
+				stringRange = append(stringRange, -1)
 			}
 		}
 
-		stringRange = append(stringRange, fmt.Sprint(i))
+		stringRange = append(stringRange, i)
 		l = i
 	}
 
 	return PageNavigation{
 		Max:      total,
-		Active:   fmt.Sprint(current),
+		Active:   current,
 		PageList: stringRange,
 	}
 }
