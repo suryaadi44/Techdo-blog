ab1fa5ae25958176dfe53567713e6631e0b61760
suryaadi <suryawarior44@gmail.com>
2022-05-14 10:46:13 +0800

    [#60] add pagination logic and static html

---
+++ pkg/utils/paginator.go
@@ -0,0 +1,35 @@
+package utils
+
+import "fmt"
+
+func Paginate(current int64, total int64) []string {
+	var stringRange []string
+	var intRange []int64
+	var delta int64 = 2
+	var i, l int64
+
+	left := current - delta
+	right := current + delta + 1
+
+	for i = 1; i <= total; i++ {
+		if i == 1 || i == total || i >= left && i < right {
+			intRange = append(intRange, i)
+		}
+
+	}
+
+	for idx, i := range intRange {
+		if idx != 0 {
+			if i-l == 2 {
+				stringRange = append(stringRange, fmt.Sprint(l+1))
+			} else if i-l != 1 {
+				stringRange = append(stringRange, "...")
+			}
+		}
+
+		stringRange = append(stringRange, fmt.Sprint(i))
+		l = i
+	}
+
+	return stringRange
+}
